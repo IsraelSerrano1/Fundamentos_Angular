d6ab6196c6f8c1baeaabc6076bd3a6ce5642f361
IsraelSerrano1 <israel.serrano@softtek.com>
2023-04-14 12:13:04 +0200

    Añado los tipos de definir String

--- src/app/variables/variables.component.ts
+++ src/app/variables/variables.component.ts
@@ -15,13 +15,20 @@ ngOnInit(): void{
 
   imprimirTipos(){
     let numero: number=1
-    let cadena: string="cadena de texto"
+    let cadenaDobles: string="cadena de texto con comillas dobles \""
+    let cadenaSimple: string='cadena de texto con comilla simple \''
+    let cadenaInvertida: string= `cadena de texto con comilla invertida \` 
+    En la que podemos dar saltos de linea
+    E insertar expresiones:
+    ${cadenaDobles}`
     let booleano: boolean=true
     let any: any=75
     let unknown: unknown="tipo desconocido"
 
     console.log("El tipo de variable es ",typeof numero," y el valor es: ", numero)
-    console.log("El tipo de variable es ",typeof cadena," y el valor es: ", cadena)
+    console.log("El tipo de variable es ",typeof cadenaDobles," y el valor es: ", cadenaDobles)
+    console.log("El tipo de variable es ",typeof cadenaSimple," y el valor es: ", cadenaSimple)
+    console.log("El tipo de variable es ",typeof cadenaInvertida," y el valor es: ", cadenaInvertida)
     console.log("El tipo de variable es ",typeof booleano," y el valor es: ", booleano)
     console.log("El tipo de variable es ",typeof any," y el valor es: ", any)
     console.log("El tipo de variable es ",typeof unknown," y el valor es: ", unknown) 
